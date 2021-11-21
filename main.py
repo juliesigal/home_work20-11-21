40bf47272bc8f6ecfbc75de2e631db7ac7c5f382
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-21 13:36:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import shelve
+
+
 class Grade:
-    def __init__(self,id,subject_name,student_id,grade):
+    def __init__(self, id, subject_name, student_id, grade):
         self.id = id
         self.subject_name = subject_name
         self.student_id = student_id
@@ -14,7 +17,7 @@ class Grade:
                f'the student id is: {self.student_id} and the student grade is: {self.grade}'
 
     def __eq__(self, other):
-        if isinstance(other,Grade):
+        if isinstance(other, Grade):
             return self.grade == other.grade
         elif type(other) in [int]:
             return self.id == other
@@ -25,7 +28,7 @@ class Grade:
         return not self.__eq__(other)
 
     def __gt__(self, other):
-        if isinstance(other,Grade):
+        if isinstance(other, Grade):
             return self.grade > other.grade
         elif type(other) in [int]:
             return self.id > other
@@ -33,7 +36,7 @@ class Grade:
             return False
 
     def __lt__(self, other):
-        if isinstance(other,Grade):
+        if isinstance(other, Grade):
             return self.grade < other.grade
         elif type(other) in [int]:
             return self.id < other
@@ -43,7 +46,19 @@ class Grade:
     def __add__(self):
         self.grade += other
         return self
-    
+
     def __sub__(self, other):
         self.grade -= other
         return self
+
+sh = shelve.open('grades.db')
+f_grade = Grade(111, 'John', 123, 75)
+s_grade = Grade(222, 'Bill', 234, 80)
+t_grade = Grade(333, 'Hellen', 345, 85)
+fr_grade = Grade(444, 'Diana', 456, 90)
+grade.id = input('please enter id: ')
+if grade.id in sh.keys():
+    gr = Grade()
+    gr.__dict__ = sh[grade.id]
+    print(gr)
+sh.close()
